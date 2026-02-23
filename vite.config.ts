7e62d25dd1aee031be5f7443fb1ed59d58e9de0b
barbosajpc <156725431+barbosajpc@users.noreply.github.com>
2026-02-23 11:27:58 -0300

    fix: vite base config

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/bepi/",
+  base: "/bepi-interativo/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
